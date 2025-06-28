4ca5420d5fb61723832a8a3b3f538437af9632e1
SpencerDuncan8 <duncans247@gmail.com>
2025-06-28 11:49:23 +0000

    Added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { AppProviders } from './context/AppContext.jsx';
+import { Analytics } from '@vercel/analytics/react'; // <-- CORRECT IMPORT FOR REACT
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,5 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AppProviders>
       <App />
     </AppProviders>
+    <Analytics /> {/* <-- ADD THE COMPONENT HERE */}
   </React.StrictMode>
 );
